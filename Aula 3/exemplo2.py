0c18487ab34a728c2f983339a4d7b77b559ef223
Murillo Prevelato <prevelatomurillo@gmail.com>
2025-02-24 20:08:32 -0300

    feito

--- Aula 3/exemplo2.py
+++ Aula 3/exemplo2.py
@@ -3,4 +3,4 @@ if salario < 1800:
     novo_salario = salario * 1.1
     print("O funcionario tem direito a 10% de aumento")
     print("O novo salario sera de R$ %.2f" % novo_salario)
-    print("Parab ns pelo novo salário!!!")
+    print("Parabéns pelo novo salário!!!")
---
+++ Aula 3/exercicio1.py
@@ -0,0 +1,9 @@
+a = int(input("Primeiro valor: "))
+b = int(input("Segundo valor: "))
+
+if a > b:
+    print(f"Maior: {a}")
+
+if b > a: 
+    print(f"Maior: {b}")
+
